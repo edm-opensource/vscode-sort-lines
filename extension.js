bbf4fb7f9f2ca12a07b8c7cd86f0aa463f65ea51
Mattias Pernhult <pernhultmattias@gmail.com>
2015-11-24 16:01:15 +0100

    Working prototype of this extensions

--- extension.js
+++ extension.js
@@ -13,13 +13,59 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	var disposable = vscode.commands.registerCommand('extension.sortLinesAsc', function () {
+		sortLines(true);
+	});
+	
+	var disposable2 = vscode.commands.registerCommand('extension.sortLinesDesc', function () {
+		sortLines(false);
 	});
 	
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
+
+function sortLines(ascendingOrder) {
+	var editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return; // No open text editor
+		}
+		
+		var end = editor.selections[0].anchor.line;
+		var start = editor.selections[0].active.line;
+		
+		if (end < start) {
+			var temp = start;
+			start = end;
+			end = temp;
+		}
+		
+		var endChar = editor.document._lines[end].length;
+		
+		var startPos = new vscode.Position(start, 0);
+		var endPos = new vscode.Position(end, endChar);
+		
+		var range = new vscode.Range(startPos, endPos);
+		
+		var textArr = editor.document._lines.slice(start, end+1);
+		
+		console.log(textArr);	
+		
+		textArr.sort();
+		if (!ascendingOrder) {
+			textArr.reverse();
+		}
+		
+		console.log(textArr);
+		
+		var replaceString = textArr[0];
+		for (var i = 1; i < textArr.length; i++) {
+			replaceString += '\n' + textArr[i];
+		}
+		var workspaceEdit = new vscode.WorkspaceEdit();
+		
+		workspaceEdit.replace(editor.document._uri, range, replaceString);
+		vscode.workspace.applyEdit(workspaceEdit);
+}
+
 exports.activate = activate;
